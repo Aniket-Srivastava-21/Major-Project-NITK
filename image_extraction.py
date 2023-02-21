af53d135e62bea1be336378fea516dc79fdfc1c5
Aniket-Srivastava-21 <60710662+Aniket-Srivastava-21@users.noreply.github.com>
2023-02-21 14:42:31 +0530

    Update image_extraction.py

--- image_extraction.py
+++ image_extraction.py
@@ -1,56 +1,40 @@
 import os
 import tensorflow.compat.v1 as tf
-import math
 import numpy as np
-import itertools
 import sys
 import matplotlib.pyplot as plt
 
 tf.enable_eager_execution()
 
-file = tarfile.open('large_dataset.tar')
-  
-# extracting file
-file.extractall('./Combined_dataset')
-
-file.close()
-
-os.remove('large_dataset.tar')
-
-directory = 'Combined_dataset'
+FILENAME = 'dataset.tfrecord'
 
 label_list = []
 image_list = []
 
-done_count = 0
+dataset = tf.data.TFRecordDataset(FILENAME, compression_type='')
 
-for filename in os.listdir(directory):
-  FILENAME = os.path.join(directory, filename)
+try:
+  for data in dataset:
+      frame = open_dataset.Frame()
+      frame.ParseFromString(bytearray(data.numpy()))
 
-  dataset = tf.data.TFRecordDataset(FILENAME, compression_type='')
+      for i in range(len(frame.images)):
+          panoptic_image = frame.images[i].camera_segmentation_label.panoptic_label
+      try:
+          label = tf.image.decode_png(panoptic_image)
+          img = tf.image.decode_jpeg(frame.images[i].image)
+          label_list.append(label.numpy())
+          image_list.append(img.numpy())
+      except:
+          pass
 
-  try:
-    for data in dataset:
-        frame = open_dataset.Frame()
-        frame.ParseFromString(bytearray(data.numpy()))
+  done_count += 1
 
-        for i in range(len(frame.images)):
-            panoptic_image = frame.images[i].camera_segmentation_label.panoptic_label
-        try:
-            label = tf.image.decode_png(panoptic_image)
-            img = tf.image.decode_jpeg(frame.images[i].image)
-            label_list.append(label.numpy())
-            image_list.append(img.numpy())
-        except:
-            pass
+except Exception as e:
+    print("Error:", e)
 
-    done_count += 1
-      
-  except Exception as e:
-      print("Error:", e)
-
-  print(FILENAME, "Done!")
-  print(len(label_list), len(image_list))
+print("Done!")
+print(len(label_list), len(image_list))
 
 len(label_list), len(image_list)
 
@@ -59,8 +43,6 @@ plt.imshow(image_list[0])
 plt.imshow(label_list[0][:,:,0])
 
 sys.getsizeof(image_list[0])
-
-5103496 * 394
 
 sys.getsizeof(label_list[0])
 
